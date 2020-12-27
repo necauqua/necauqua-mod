70e62b96d13d6c4022b58a0ff304bd177a0df8b0
Anton Bulakh <self@necauqua.dev>
2020-12-27 04:46:55 +0200

    Don't completely remove Maven publications and stuff if project has no credentials, only remove the repo description

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,37 +25,37 @@ dependencies {
     implementation("com.matthewprenger.cursegradle:com.matthewprenger.cursegradle.gradle.plugin:1.4.0")
 }
 
-if (listOf("maven.user", "maven.pass").all(project::hasProperty)) {
-    publishing {
-        publications {
-            create<MavenPublication>("main") {
-                from(components["java"])
-                pom {
-                    name.set("necauqua-mod")
-                    description.set("A dumb Gradle plugin that sets up all the defaults related to making a Minecraft mod if you are *me*")
+publishing {
+    publications {
+        create<MavenPublication>("main") {
+            from(components["java"])
+            pom {
+                name.set("necauqua-mod")
+                description.set("A dumb Gradle plugin that sets up all the defaults related to making a Minecraft mod if you are *me*")
+                url.set("https://github.com/necauqua/necauqua-mod#readme")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/mit-license")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("necauqua")
+                        name.set("Anton Bulakh")
+                        email.set("self@necauqua.dev")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/necauqua/necauqua-mod")
+                    developerConnection.set("scm:git:https://github.com/necauqua/necauqua-mod")
                     url.set("https://github.com/necauqua/necauqua-mod#readme")
-                    licenses {
-                        license {
-                            name.set("MIT License")
-                            url.set("https://opensource.org/licenses/mit-license")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("necauqua")
-                            name.set("Anton Bulakh")
-                            email.set("self@necauqua.dev")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:https://github.com/necauqua/necauqua-mod")
-                        developerConnection.set("scm:git:https://github.com/necauqua/necauqua-mod")
-                        url.set("https://github.com/necauqua/necauqua-mod#readme")
-                    }
                 }
             }
         }
-        repositories {
+    }
+    repositories {
+        if (listOf("maven.user", "maven.pass").all(project::hasProperty)) {
             maven {
                 name = "necauqua"
                 url = uri("https://maven.necauqua.dev")
